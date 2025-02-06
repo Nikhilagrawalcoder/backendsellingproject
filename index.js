157842b8c38a4dadfca327240facead678c5bed8
NIK0605 <nikhilagrawal0605@gmail.com>
2025-02-06 22:54:25 +0530

    add

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const express=require('express')
 const app=express()
+const Logger=require('log-vani')
+const logger=new Logger();
+
+
+app.post("/user/signup",(req,res)=>{
+    res.json({msg:'signup succesfully'})
+})
+
+app.post("/user/login",(req,res)=>{
+    res.json({msg:'login succesfully'})
+})
+
+app.get("/courses",(req,res)=>{
+    res.json({msg:'all courses'})
+})
+
+app.get("/user/purchase",(req,res)=>{
+    res.json({msg:'all courses that are purchased'})
+})
+
+app.post("/user/purchase",(req,res)=>{
+    res.json({msg:'succesfully purchase course'})
+})
+app.listen(8080,(err)=>{
+    logger.info('server stareted succesfully')
+})
